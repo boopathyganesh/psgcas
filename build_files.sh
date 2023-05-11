8aae5b08aad221d9b70c97ee83790364f5ae53bc
Boopathy Ganesh K <47422799+boopathyganesh@users.noreply.github.com>
2023-05-11 16:15:17 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,3 +4,11 @@ pip install -r requirements.txt
 # make migrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
+
+#linux update and install
+
+apt-get update
+apt-get upgrade
+apt install mysql-server libmysqlclient-dev build-essential python3-dev
+
+export PATH="/usr/local/bin:$PATH"
